213970ef8b2b3ec7e481056c6f6dc42caf7c04c0
Joaquin Navarro <inavarrojoaquin@gmail.com>
2022-07-11 08:16:47 +0200

    Is a Leap Year full implementation

---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,16 @@
+﻿namespace LeapYear
+{
+    public class LeapYear
+    {
+        public bool Execute(int year)
+        {
+            if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0)
+                return false;
+
+            if (year % 4 != 0)
+                return false;
+            
+            return true;
+        }
+    }
+}
--- LeapYear/LeapYearShould.cs
+++ LeapYear/LeapYearShould.cs
@@ -4,15 +4,46 @@ namespace LeapYear
 {
     public class LeapYearShould
     {
+        private LeapYear leapYear;
+
         [SetUp]
         public void Setup()
         {
+            leapYear = new LeapYear();
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(1996)]
+        [TestCase(2012)]
+        [TestCase(2020)]
+        public void BeDivisibleByFour(int year)
         {
-            Assert.Pass();
+            Assert.IsTrue(leapYear.Execute(year));
+        }
+
+        [TestCase(1997)]
+        [TestCase(1999)]
+        [TestCase(2001)]
+        public void NotBeDivisibleByFour(int year)
+        {
+            Assert.IsFalse(leapYear.Execute(year));
+        }
+
+        [TestCase(1600)]
+        [TestCase(2000)]
+        [TestCase(2400)]
+        public void BeDivisibleByFourHundred(int year)
+        {
+            Assert.IsTrue(leapYear.Execute(year));
+        }
+
+        [TestCase(1800)]
+        [TestCase(1900)]
+        [TestCase(2100)]
+        [TestCase(2200)]
+        [TestCase(2300)]
+        public void BeDivisibleByFourHundredByOneHundredButNotByFourHundred(int year)
+        {
+            Assert.IsFalse(leapYear.Execute(year));
         }
     }
 }
